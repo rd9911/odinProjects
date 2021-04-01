ca298f9300602beeaf4f3b0811a2555b2cfb61e6
Avazjon Yusufjonov <azizaziz9911@mail.ru>
2021-04-01 23:14:48 +0900

    Create sketch of style of calculator

--- calculator/index.js
+++ calculator/index.js
@@ -1,15 +1,24 @@
-const grids = document.createElement('ul');
+const container = document.querySelector('#container');
+const grids = document.createElement('div');
 grids.id = 'grids';
 const idOfElement = [
     'display', 'clear', 'factor', 'percentile', 'divide', 'seven', 'eight', 
     'nine', 'multiply', 'four', 'five', 'six', 'subtract', 'one', 'two', 
     'three', 'add', 'zero', 'floating', 'calculate']
+const textOfElement = [
+    '', 'AC', '!', '%', '/', '7', '8', '9', '*',
+    '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='
+]
+
+
+container.style.setProperty('--grid-rows', 6);
+container.style.setProperty('--grid-cols', 4);
 for(let i=0; i < idOfElement.length; i++) {
-    let li = document.createElement('li');
-    li.id = idOfElement[i];
-    // add attributes and styles.
+    let btn = document.createElement('div');
+    btn.id = idOfElement[i];
+    btn.innerHTML = textOfElement[i];
+    // add attributes and styles
+    grids.appendChild(btn).classList = 'grid-item';
 };
 
-const container = document.querySelector('#container');
-container.appendChild(grid);
-
+container.appendChild(grids);
