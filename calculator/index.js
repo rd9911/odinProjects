2f8eb5984b9f591f444e91946845ac61c71d62e7
Avazjon Yusufjonov <azizaziz9911@mail.ru>
2021-04-03 17:27:47 +0900

    Change style of buttons and Add more styling

--- calculator/index.js
+++ calculator/index.js
@@ -1,24 +1,37 @@
 const container = document.querySelector('#container');
 const grids = document.createElement('div');
 grids.id = 'grids';
+
+
+const displayOfCalculator = document.createElement('div');
+displayOfCalculator.id = 'display';
+const textPanel = document.createElement('h3');
+textPanel.id = 'text-panel';
+displayOfCalculator.appendChild(textPanel);
+container.appendChild(displayOfCalculator);
+
+
 const idOfElement = [
-    'display', 'clear', 'factor', 'percentile', 'divide', 'seven', 'eight', 
+    'clear', 'factor', 'percentile', 'divide', 'seven', 'eight', 
     'nine', 'multiply', 'four', 'five', 'six', 'subtract', 'one', 'two', 
-    'three', 'add', 'zero', 'floating', 'calculate']
+    'three', 'add', 'zero', 'brackets', 'floating', 'calculate']
 const textOfElement = [
-    '', 'AC', '!', '%', '/', '7', '8', '9', '*',
-    '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='
+    'AC', '!', '%', '/', '7', '8', '9', '*',
+    '4', '5', '6', '-', '1', '2', '3', '+', '0', '()', '.', '='
 ]
 
 
 container.style.setProperty('--grid-rows', 6);
 container.style.setProperty('--grid-cols', 4);
 for(let i=0; i < idOfElement.length; i++) {
-    let btn = document.createElement('div');
+    let btn = document.createElement('button');
     btn.id = idOfElement[i];
     btn.innerHTML = textOfElement[i];
+    btn.type = 'button'
     // add attributes and styles
     grids.appendChild(btn).classList = 'grid-item';
 };
 
 container.appendChild(grids);
+
+// add EventListeners
